634b1ba86268dcb0f76813b62ce38629b2768198
Andrey Rezanovich <and.rezanovich@gmail.com>
2020-03-24 10:15:45 +0200

    returns todo by: id, error handling

--- app.js
+++ app.js
@@ -1,41 +1,29 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const todos = require('./data/db');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = express();
 
-var app = express();
+/* Выводит дынные с файла db.js */
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.get('/api/todos/', (req, res, next) => {
+    res.send({todos: todos})
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+/* Выводит конкретную туду по id */
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.get('/api/todos/:id', (req, res, next) => {
+    let foundTodo = todos.find((todo) => {
+        return todo.id === req.params.id;
+    });
+    if (foundTodo) {
+        res.status(200).send(foundTodo);
+    } else {
+        res.status(404).send(`Page with ID: ` + req.params.id + ` not found`)
+    }
 });
 
-module.exports = app;
+/* Запуск сервера */
+
+app.listen(3000, (req, res) => {
+    console.log('server works');
+});
---
+++ data/db.js
@@ -0,0 +1,19 @@
+const todos = [
+  {
+    id: '0',
+    text: 'First todo'
+  },
+  {
+    id: '1',
+    text: 'Second todo'
+  },
+  {
+    id: '2',
+    text: 'Third todo'
+  }
+];
+
+
+module.exports = todos;
+
+// route.get.by.id;
